aa74e162e44d9a4d56463d2e18e57ebd9a7dd182
Herman Hiddema <h.hiddema@paytsoftware.com>
2026-02-25 18:28:09 +0100

    Move pages to static content controller (#16)
    * Move pages to static content controller
    
    * Add newline at end of static_content_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -17,68 +17,68 @@ Rails.application.routes.draw do
   resources :articles
 
   # Static pages for menu
-  get "about" => "pages#about", as: :about
-  get "schedule" => "pages#schedule", as: :schedule
-  get "venue" => "pages#venue", as: :venue
-  get "contact" => "pages#contact", as: :contact
+  get "about" => "static_content#about", as: :about
+  get "schedule" => "static_content#schedule", as: :schedule
+  get "venue" => "static_content#venue", as: :venue
+  get "contact" => "static_content#contact", as: :contact
 
   # Go Tournaments
-  get "go-tournaments/egc-rules" => "pages#egc_rules", as: :egc_rules
-  get "go-tournaments/european-championship" => "pages#european_championship", as: :european_championship
-  get "go-tournaments/main-open" => "pages#main_open", as: :main_open
-  get "go-tournaments/weekend" => "pages#weekend_tournament", as: :weekend_tournament
-  get "go-tournaments/pandanet" => "pages#pandanet", as: :pandanet
-  get "go-tournaments/rapid" => "pages#rapid", as: :rapid
-  get "go-tournaments/senior" => "pages#senior", as: :senior
-  get "go-tournaments/youth" => "pages#youth", as: :youth
-  get "go-tournaments/pair-go" => "pages#pair_go", as: :pair_go
-  get "go-tournaments/marathon-9x9" => "pages#marathon_9x9", as: :marathon_9x9
-  get "go-tournaments/marathon-13x13" => "pages#marathon_13x13", as: :marathon_13x13
-  get "go-tournaments/blitz" => "pages#blitz", as: :blitz
-  get "go-tournaments/women" => "pages#women", as: :women
-  get "go-tournaments/teams" => "pages#teams", as: :teams
-  get "go-tournaments/tsume-go" => "pages#tsume_go", as: :tsume_go
-  get "go-tournaments/chess-and-go" => "pages#chess_and_go", as: :chess_and_go
-  get "go-tournaments/beer-and-go" => "pages#beer_and_go", as: :beer_and_go
-  get "go-tournaments/torus-go" => "pages#torus_go", as: :torus_go
-  get "go-tournaments/hexgo" => "pages#hexgo", as: :hexgo
+  get "go-tournaments/egc-rules" => "static_content#egc_rules", as: :egc_rules
+  get "go-tournaments/european-championship" => "static_content#european_championship", as: :european_championship
+  get "go-tournaments/main-open" => "static_content#main_open", as: :main_open
+  get "go-tournaments/weekend" => "static_content#weekend_tournament", as: :weekend_tournament
+  get "go-tournaments/pandanet" => "static_content#pandanet", as: :pandanet
+  get "go-tournaments/rapid" => "static_content#rapid", as: :rapid
+  get "go-tournaments/senior" => "static_content#senior", as: :senior
+  get "go-tournaments/youth" => "static_content#youth", as: :youth
+  get "go-tournaments/pair-go" => "static_content#pair_go", as: :pair_go
+  get "go-tournaments/marathon-9x9" => "static_content#marathon_9x9", as: :marathon_9x9
+  get "go-tournaments/marathon-13x13" => "static_content#marathon_13x13", as: :marathon_13x13
+  get "go-tournaments/blitz" => "static_content#blitz", as: :blitz
+  get "go-tournaments/women" => "static_content#women", as: :women
+  get "go-tournaments/teams" => "static_content#teams", as: :teams
+  get "go-tournaments/tsume-go" => "static_content#tsume_go", as: :tsume_go
+  get "go-tournaments/chess-and-go" => "static_content#chess_and_go", as: :chess_and_go
+  get "go-tournaments/beer-and-go" => "static_content#beer_and_go", as: :beer_and_go
+  get "go-tournaments/torus-go" => "static_content#torus_go", as: :torus_go
+  get "go-tournaments/hexgo" => "static_content#hexgo", as: :hexgo
 
   # Other Activities
-  get "activities/opening-ceremony" => "pages#opening_ceremony", as: :opening_ceremony
-  get "activities/group-photo" => "pages#group_photo", as: :group_photo
-  get "activities/egf-meeting" => "pages#egf_meeting", as: :egf_meeting
-  get "activities/conference" => "pages#conference", as: :conference
-  get "activities/prizegivings" => "pages#prizegivings", as: :prizegivings
-  get "activities/game-reviews" => "pages#game_reviews", as: :game_reviews
-  get "activities/simultaneous-games" => "pages#simultaneous_games", as: :simultaneous_games
-  get "activities/lectures" => "pages#lectures", as: :lectures
-  get "activities/tsume-go-activity" => "pages#tsume_go_activity", as: :tsume_go_activity
-  get "activities/poker" => "pages#poker", as: :poker
-  get "activities/other-games" => "pages#other_games", as: :other_games
-  get "activities/sport" => "pages#sport", as: :sport
-  get "activities/excursions/organised-local" => "pages#excursions_organised_local", as: :excursions_organised_local
-  get "activities/excursions/organised-out-of-town" => "pages#excursions_organised_out_of_town", as: :excursions_organised_out_of_town
-  get "activities/excursions/diy-local" => "pages#excursions_diy_local", as: :excursions_diy_local
-  get "activities/excursions/diy-out-of-town" => "pages#excursions_diy_out_of_town", as: :excursions_diy_out_of_town
+  get "activities/opening-ceremony" => "static_content#opening_ceremony", as: :opening_ceremony
+  get "activities/group-photo" => "static_content#group_photo", as: :group_photo
+  get "activities/egf-meeting" => "static_content#egf_meeting", as: :egf_meeting
+  get "activities/conference" => "static_content#conference", as: :conference
+  get "activities/prizegivings" => "static_content#prizegivings", as: :prizegivings
+  get "activities/game-reviews" => "static_content#game_reviews", as: :game_reviews
+  get "activities/simultaneous-games" => "static_content#simultaneous_games", as: :simultaneous_games
+  get "activities/lectures" => "static_content#lectures", as: :lectures
+  get "activities/tsume-go-activity" => "static_content#tsume_go_activity", as: :tsume_go_activity
+  get "activities/poker" => "static_content#poker", as: :poker
+  get "activities/other-games" => "static_content#other_games", as: :other_games
+  get "activities/sport" => "static_content#sport", as: :sport
+  get "activities/excursions/organised-local" => "static_content#excursions_organised_local", as: :excursions_organised_local
+  get "activities/excursions/organised-out-of-town" => "static_content#excursions_organised_out_of_town", as: :excursions_organised_out_of_town
+  get "activities/excursions/diy-local" => "static_content#excursions_diy_local", as: :excursions_diy_local
+  get "activities/excursions/diy-out-of-town" => "static_content#excursions_diy_out_of_town", as: :excursions_diy_out_of_town
 
   # Eat and Drink
-  get "eat-and-drink/go-coins" => "pages#go_coins", as: :go_coins
-  get "eat-and-drink/meals/vip-dinner" => "pages#vip_dinner", as: :vip_dinner
-  get "eat-and-drink/meals/bbq-saturday" => "pages#bbq_saturday", as: :bbq_saturday
-  get "eat-and-drink/meals/onsite-meals" => "pages#onsite_meals", as: :onsite_meals
-  get "eat-and-drink/meals/local-restaurants" => "pages#local_restaurants", as: :local_restaurants
-  get "eat-and-drink/meals/local-bars" => "pages#local_bars", as: :local_bars
+  get "eat-and-drink/go-coins" => "static_content#go_coins", as: :go_coins
+  get "eat-and-drink/meals/vip-dinner" => "static_content#vip_dinner", as: :vip_dinner
+  get "eat-and-drink/meals/bbq-saturday" => "static_content#bbq_saturday", as: :bbq_saturday
+  get "eat-and-drink/meals/onsite-meals" => "static_content#onsite_meals", as: :onsite_meals
+  get "eat-and-drink/meals/local-restaurants" => "static_content#local_restaurants", as: :local_restaurants
+  get "eat-and-drink/meals/local-bars" => "static_content#local_bars", as: :local_bars
 
   # Sleep
-  get "sleep/hotels" => "pages#hotels", as: :hotels
-  get "sleep/camping" => "pages#camping", as: :camping
-  get "sleep/budget" => "pages#budget_accommodation", as: :budget_accommodation
+  get "sleep/hotels" => "static_content#hotels", as: :hotels
+  get "sleep/camping" => "static_content#camping", as: :camping
+  get "sleep/budget" => "static_content#budget_accommodation", as: :budget_accommodation
 
   # Who is here
-  get "who-is-here/participants" => "pages#participants", as: :participants
-  get "who-is-here/teachers" => "pages#teachers", as: :teachers
-  get "who-is-here/shops" => "pages#shops", as: :shops
-  get "who-is-here/exhibitors" => "pages#exhibitors", as: :exhibitors
+  get "who-is-here/participants" => "static_content#participants", as: :participants
+  get "who-is-here/teachers" => "static_content#teachers", as: :teachers
+  get "who-is-here/shops" => "static_content#shops", as: :shops
+  get "who-is-here/exhibitors" => "static_content#exhibitors", as: :exhibitors
 
   root "home#index"
 end
