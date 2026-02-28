e298be0f83611cfddb9be7a40f48e9d62c1d4b95
Herman Hiddema <h.hiddema@paytsoftware.com>
2026-02-28 22:47:12 +0100

    Some cleanup (#26)
    * Cleanup
    
    * Don't show slug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,7 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:style_guide, :index, :debug]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @recent_articles = Article.order(created_at: :desc).limit(3).includes(:user)
-  end
-
-  def style_guide; end
-
-  def debug
-    @user_count = User.count
-    @test_user = User.find_by(email: "test@example.com")
-    @user_signed_in = user_signed_in?
-    @current_user = current_user if user_signed_in?
   end
 end
