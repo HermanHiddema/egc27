e1edb888a0d399161cbd24ec63e0ec25b44259f6
Herman Hiddema <h.hiddema@paytsoftware.com>
2026-03-14 13:06:52 +0000

    work on the devcontainer branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_03_11_050000) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -28,7 +31,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_11_050000) do
     t.datetime "starts_at", null: false
     t.string "title", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["ends_at"], name: "index_events_on_ends_at"
     t.index ["starts_at"], name: "index_events_on_starts_at"
     t.index ["user_id"], name: "index_events_on_user_id"
@@ -37,10 +40,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_11_050000) do
   create_table "menu_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "label", null: false
-    t.integer "menu_id", null: false
+    t.bigint "menu_id", null: false
     t.boolean "open_in_new_tab", default: false, null: false
-    t.integer "page_id"
-    t.integer "parent_id"
+    t.bigint "page_id"
+    t.bigint "parent_id"
     t.integer "position", default: 0, null: false
     t.datetime "updated_at", null: false
     t.string "url"
