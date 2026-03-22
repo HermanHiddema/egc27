0cdd8119bc589e8cac13c4520aed4f41bdd129fc
Herman Hiddema <h.hiddema@paytsoftware.com>
2026-03-22 21:14:01 +0000

    Responsive menu

---
+++ app/javascript/controllers/responsive_menu_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["panel", "toggle"]
+
+  connect() {
+    this.close()
+  }
+
+  togglePanel() {
+    if (this.panelTarget.classList.contains("hidden")) {
+      this.open()
+      return
+    }
+
+    this.close()
+  }
+
+  open() {
+    this.panelTarget.classList.remove("hidden")
+    this.toggleTarget.setAttribute("aria-expanded", "true")
+  }
+
+  close() {
+    this.panelTarget.classList.add("hidden")
+    this.toggleTarget.setAttribute("aria-expanded", "false")
+  }
+}
